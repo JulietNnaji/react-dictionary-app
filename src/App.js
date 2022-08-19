d56b3854f767b064f574011faa7b232e2eb9962d
Juliet Nnaji <98786226+JulietNnaji@users.noreply.github.com>
2022-08-19 18:43:42 +0100

    Added an horizontal line

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 export default function App() {
   return (
     <div className ="container">
-    <div className="box">
       <div className ="App">
       <h1>
  Dictionary
@@ -14,7 +13,7 @@ export default function App() {
  <div>
 <Dictionary />
 </div>
-</div>
+<hr />
 <footer  className ="owner">
   This App is <a href ="https://github.com/JulietNnaji/react-dictionary-app" target= "_blank" rel = "noreferrer"> open-sourced </a> and coded by <a href ="https://github.com/JulietNnaji" target= "_blank" rel = "noreferrer"> Juliet Nnaji </a>
 </footer>
